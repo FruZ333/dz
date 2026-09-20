a0c46f80e2c9db32f968e5a7b43331edd77388ac
FruZ333 <>
2026-09-20 21:39:54 +0600

    postgres

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"context"
+	"dz/postgres-sql/connection"
+	"fmt"
+)
+
 func main() {
-	
+	ctx := context.Background()
+
+	conn, err := connection.ConnectionDB(ctx)
+	if err != nil {
+		fmt.Println("error:", err)
+	} 
 }
